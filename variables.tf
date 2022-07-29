f6d88c754b9fc260de3ad297c573cc95438c1d50
rhemazoe <81827898+rhemazoe@users.noreply.github.com>
2022-07-29 01:54:34 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,19 +1,16 @@
 variable "region" {
     description = "Default Region for Provider"
     type        = string 
-    default     = "us-east-1"
 }
 
 variable "bucket" {
     description = "s3 Bucket Name"
     type        = string 
-    default     = "pivotbio"
 }
 
 
 variable "name" {
     description = "sns topic name"
     type        = string 
-    default     = "pivotbio-notify"
 }
 
